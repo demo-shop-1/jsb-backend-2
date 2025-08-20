8eabd0e28439fd634e135f4e68035d4a4dc8ed37
Willy Arrojas <willyarrojas@gmail.com>
2025-08-20 17:22:50 +0200

    add project java spring boot

---
+++ mocks/02-create-schema.sql
@@ -0,0 +1,7 @@
+-- -----------------------------------------------------
+-- Schema demo-shop-1
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `demo-shop-1`;
+
+CREATE SCHEMA `demo-shop-1`;
+USE `demo-shop-1` ;
